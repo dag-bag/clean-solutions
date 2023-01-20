ffa8ab76bd44ba2b916b99fc8f85beec18b5c5b2
dag-bag <virenderkumar23435@gmail.com>
2023-01-20 21:15:09 +0530

    Fix Next Prev Button Bug

--- atoms/steps.ts
+++ atoms/steps.ts
@@ -1,5 +1,5 @@
 /** @format */
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 export const stepAtom = atom({
   key: "stepAtom",
   default: 0,
@@ -8,4 +8,20 @@ export const stepAtom = atom({
 export const disAbleAtom = atom<boolean>({
   key: "disAbleAtom",
   default: false,
+});
+
+export const stepSelector = selector({
+  key: "stepSelector",
+  get: ({ get }) => {
+    const step = get(stepAtom);
+    return step === 0
+      ? "outer"
+      : step === 1
+      ? "outer"
+      : step === 2
+      ? "inner"
+      : step === 3
+      ? "deep"
+      : "outer";
+  },
 });
