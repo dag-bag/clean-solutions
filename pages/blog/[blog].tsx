5929bd1c46c38a38a9b299152b2ee00e90e55a98
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-02-03 21:22:05 +0530

    blog designed

--- pages/blog/[blog].tsx
+++ pages/blog/[blog].tsx
@@ -1,22 +1,33 @@
 /** @format */
-
 import React from "react";
-
+import Head from "next/head";
+import Navbar from "../../components/Navbar";
 type Props = {
   post: PostsInterFace;
 };
-import marked from "marked";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { PostsInterFace } from "../../types/blogs";
 function BlogsDetailsPage({ post }: Props) {
-  console.log("post:", post);
   return (
-    <div
-      className=" mx-auto text-center  max-w-6xl prose py-10 md:prose-xl"
-      dangerouslySetInnerHTML={{
-        __html: post.content.rendered,
-      }}
-    ></div>
+    <>
+      <Head>
+        <title>{post.title.rendered}</title>
+      </Head>
+      <>
+        <Navbar />
+        <div className=" p-5 md:p-0 text-4xl md:text-6xl   h-[500px] flex items-center justify-center  bg-[#F2F3F9]">
+          <h1 className="max-w-6xl font-heading font-extrabold text-blue-1 ">{post.title.rendered}</h1>
+        </div>
+        <div
+          className="p-5 mx-auto text-center max-w-6xl prose py-10 md:prose-xl prose-headings:font-heading prose-h1:font-bold prose-p:text-left  prose-p:text-gray-500 prose-p:my-0 prose-h1:py-10 prose-table:border-gray-100 prose-table:border-2 prose-a:text-sm prose-a:text-blue-500 "
+
+          dangerouslySetInnerHTML={{
+            __html: post.content.rendered,
+          }}
+        ></div>
+      </>
+
+    </>
   );
 }
 
