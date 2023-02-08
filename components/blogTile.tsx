9680f53b62c852fd4a312b1eab4166186d53ef48
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-02-08 22:52:51 +0530

    update blogTile with Nextjs image

--- components/blogTile.tsx
+++ components/blogTile.tsx
@@ -1,6 +1,6 @@
 import Router from "next/router"
 import { PostsInterFace } from "../types/blogs"
-
+import Image from "next/image"
 const BlogTile: React.FC<PostsInterFace> = (props) => {
     const paragraph = props.excerpt.rendered.replace('[&#8230;]', '...')
     function onReadMoreBtnClickHandler() {
@@ -8,10 +8,10 @@ const BlogTile: React.FC<PostsInterFace> = (props) => {
     }
     return (
         <div className="grid lg:grid-cols-[450px_auto] mb-5 bg-gray-100  overflow-hidden shadow">
-            <div>
-                <img className="w-full h-full object-center" src={props._embedded["wp:featuredmedia"][0].link} alt={'image'} />
+            <div className="image-container">
+                <Image src={props._embedded["wp:featuredmedia"][0].link} layout="fill" alt="any" className={'image'} />
             </div>
-            <div className="p-5">
+            <div className="p-10">
                 <h1 className="text-2xl font-semibold mb-5 ">{props.title.rendered}</h1>
                 <p className="text-gray-500 text-md" dangerouslySetInnerHTML={{ __html: paragraph, }}></p>
                 <button onClick={onReadMoreBtnClickHandler} className=" mt-5 items-center w-[150px] justify-start py-2 overflow-hidden  text-sm transition-all duration-150 ease-in-out rounded-full group border my-2 font-normal  border-black  hover:opacity-40  "> Read More → </button>
