91de704a1278238f146651ec088cd1183b65ccb1
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-02-13 14:48:41 +0530

    Update [blog].tsx

--- pages/blog/[blog].tsx
+++ pages/blog/[blog].tsx
@@ -18,7 +18,11 @@ function BlogsDetailsPage({ post }: Props) {
       <>
         <Navbar />
         <div className=" p-5 md:p-0 text-4xl md:text-6xl   h-[500px] flex items-center justify-center  bg-[#F2F3F9]">
-          <h1 className="max-w-6xl font-heading font-extrabold text-blue-1 ">{post.title.rendered}</h1>
+          <h1
+            dangerouslySetInnerHTML={{
+              __html: post.title.rendered,
+            }}
+            className="max-w-6xl font-heading font-extrabold text-blue-1 ">{post.title.rendered}</h1>
         </div>
         <div
           className="p-5 mx-auto text-center max-w-6xl prose py-10 md:prose-xl prose-headings:font-heading prose-h1:font-bold prose-p:text-left  prose-p:text-gray-500 prose-p:my-0 prose-h1:py-10 prose-table:border-gray-100 prose-table:border-2 prose-a:text-sm prose-a:text-blue-500 "
