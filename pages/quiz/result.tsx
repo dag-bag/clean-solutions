c213f3e7c5b1097a9338114219edbad00b2fc04a
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-02-17 13:19:23 +0530

    TTF : result page white screen

--- pages/quiz/result.tsx
+++ pages/quiz/result.tsx
@@ -1,47 +1,13 @@
 /** @format */
 import Image from "next/image";
-import useSWR from "swr";
-import { useState } from "react";
-import { useRecoilValue } from "recoil";
-import Navbar from "../../components/Navbar";
-import categoryState from "../../quiz/state";
-import Drawer from "../../components/utils/Drawer";
-import { PostsInterFace } from "../../types/blogs";
-import { GetPostsFromNextJsApi } from "../../services/blogs";
-import Loading from "../../components/Loading";
-
 import dynamic from "next/dynamic";
-
+import Navbar from "../../components/Navbar";
+import Drawer from "../../components/utils/Drawer";
+import Loading from "../../components/Loading";
 const DynamicBlogs = dynamic(() => import('../../components/blogs-section'), {
-  loading: () => <Loading />
+  loading: () => <Loading contain />
 })
-
-
-
 const ResultPage = () => {
-  // const { data } = useSWR<PostsInterFace[]>(
-  //   "/blogs",
-  //   GetPostsFromNextJsApi
-  // );
-  const state = useRecoilValue(categoryState);
-  const [toggles, setToggles] = useState({
-    categories: false,
-  });
-
-  // if (!data) {
-  //   return (
-  //     <div className="w-screen h-screen absolute top-0 left-0 flex items-center justify-center">
-  //       <div className="inline-flex items-center px-4 py-2 font-[500] leading-6 text-2xl  transition ease-in-out duration-150 cursor-not-allowed">
-  //         <svg className="animate-spin mr-5 h-10 w-10 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-  //           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-  //           <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-  //         </svg >
-  //         Generating Your Report
-  //       </div >
-  //     </div >
-  //   )
-  // }
-
   return (
     <>
       <Navbar />
@@ -68,12 +34,9 @@ const ResultPage = () => {
           <div className="md:grid md:place-items-center hidden ">
             <Image src={'/page.png'} alt="any" width={400} height={400} />
           </div>
-
         </div>
       </section >
-
       <DynamicBlogs />
-
     </>
   );
 };
